2ac9c919a81e73098ede673028283b8d30e376e5
Leonardo Mendoza <leoenmen@espol.edu.ec>
2019-12-09 17:26:07 -0500

    Consulta dinamica de productos, agregacion de la entidad Productos, dom dom dom dom dom dom

---
+++ angular-global-impressions/src/Productos.ts
@@ -0,0 +1,67 @@
+export class Productos {
+
+    private _detalle: string;
+    private _tipo: string;
+    private _precio: number;
+    private _image_path: string;
+
+
+    
+    constructor(_detalle:string, _tipo:string, _precio:number, _image_path:string){
+        this._detalle = _detalle;
+        this._tipo = _tipo;
+        this._precio = _precio;
+        this._image_path = _image_path;
+    }
+  
+    set detalle(_detalle:string) {this._detalle = _detalle;}
+    get detalle() {return this._detalle;}
+  
+    set tipo(_tipo:string) {this._tipo = _tipo;}
+    get tipo() {return this._tipo;}
+
+    set precio(_precio:number) {this._precio = _precio;}
+    get precio() {return this._precio;}
+  
+    set image_path(_image_path:string) {this._image_path = _image_path;}
+    get image_path() {return this._image_path;}
+    
+    renderizarPlantilla():string {
+      return `
+          <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+					<!-- Block2 -->
+					<div class="block2">
+						<div class="block2-pic hov-img0">
+							<img src="${this.image_path}" alt="IMG-PRODUCT">
+
+							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+								Vista Previa
+							</a>
+						</div>
+
+						<div class="block2-txt flex-w flex-t p-t-14">
+							<div class="block2-txt-child1 flex-col-l ">
+								<a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+									${this.detalle}
+								</a>
+
+								<span class="stext-105 cl3">
+									${this.precio}
+								</span>
+							</div>
+
+							<div class="block2-txt-child2 flex-r p-t-3">
+								<a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+									<img class="icon-heart1 dis-block trans-04" src="assets/icons/icon-heart-01.png" alt="ICON">
+									<img class="icon-heart2 dis-block trans-04 ab-t-l" src="assets/icons/icon-heart-02.png" alt="ICON">
+								</a>
+							</div>
+						</div>
+					</div>
+                </div>
+            </div>
+          
+          `
+    }
+  
+  }
--- angular-global-impressions/src/app/secundarias/products/products.component.ts
+++ angular-global-impressions/src/app/secundarias/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Productos } from "../../../Productos";
 
 @Component({
   selector: 'app-products',
@@ -10,6 +11,19 @@ export class ProductsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    fetch('http://my-json-server.typicode.com/leomk1998/testd1/productos')
+    .then( response => response.json())
+    .then( data => {
+    let arreglo = data;
+  
+    for (let objeto of arreglo) {
+      let producto:Productos = new Productos(objeto["detalle"], objeto["tipo"], objeto["precio"],  objeto["image_path"])
+      document.getElementById("products").innerHTML += producto.renderizarPlantilla()
+  }
+})
+.catch(function(error) {
+  console.log('Hubo un problema con la petición Fetch:' + error.message);
+});
   }
 
 }
