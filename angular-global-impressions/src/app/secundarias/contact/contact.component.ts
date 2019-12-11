3bb018ffa136e1dbf02fc117b374a1b31c1dd85f
Pabelco Zambrano <pagazamb@espol.edu.ec>
2019-12-11 11:20:55 -0500

    Completada la página de contacto
    Se colocó el mapa en contact, además en la página about ya se está adecuando para que sea responsive.

--- angular-global-impressions/src/app/secundarias/contact/contact.component.ts
+++ angular-global-impressions/src/app/secundarias/contact/contact.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  zoom: number = 8;
+  lat: number = -2.1985391;
+  lng: number = -79.8809507;
+  markers: marker[] = [
+	  {
+		  lat: -2.1985391,
+		  lng: -79.8809507,
+		  label: 'Local Malecon',
+	  },
+	  {
+		  lat: -2.1376476,
+		  lng: -79.9063379,
+		  label: 'Local Alborada',
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
 }
