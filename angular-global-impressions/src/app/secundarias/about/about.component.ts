c3029c370836a606e84f4e9505ea7232fb31c54e
Pabelco Zambrano <pagazamb@espol.edu.ec>
2019-12-11 10:42:49 -0500

    Mapa en la pagina de About Completada
    Esta lista. Falta en la página de contactanos.

--- angular-global-impressions/src/app/secundarias/about/about.component.ts
+++ angular-global-impressions/src/app/secundarias/about/about.component.ts
@@ -6,18 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  latitude = 51.678418;
-  longitude = 7.809007;
-  locationChosen = false;
-
-  onChoseLocation(event) {
-    this.latitude = event.coords.lat;
-    this.longitude = event.coords.lng;
-    this.locationChosen = true;
-  }
+  zoom: number = 8;
+  lat: number = -2.1985391;
+  lng: number = -79.8809507;
+  markers: marker[] = [
+	  {
+		  lat: -2.1985391,
+		  lng: -79.8809507,
+		  label: 'Local Malecon',
+	  },
+	  {
+		  lat: -2.1376476,
+		  lng: -79.9063379,
+		  label: 'Local Alborada',
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+}
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
 }
