a89cef8d3fd023a6030241b20d1988e7130e54bf
Leonardo Mendoza <leoenmen@espol.edu.ec>
2019-12-11 07:30:51 -0500

    Cambios ventana principal

--- angular-global-impressions/src/app/index/main/main.component.ts
+++ angular-global-impressions/src/app/index/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Productos } from "../../../Productos";
 
 @Component({
   selector: 'app-main',
@@ -10,6 +11,19 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      fetch('http://my-json-server.typicode.com/leomk1998/testd1/productos')
+      .then( response => response.json())
+      .then( data => {
+      let arreglo = data;
+    
+      for (let i=0;i<4;i++) {
+        let producto:Productos = new Productos(data[i]["detalle"], data[i]["tipo"], data[i]["precio"], data[i]["image_path"])
+        document.getElementById("products").innerHTML += producto.renderizarPlantilla()
+    }
+  })
+  .catch(function(error) {
+    console.log('Hubo un problema con la petición Fetch:' + error.message);
+  });
   }
 
 }
