28d749714c5c4fd933fc1208ee1cb8b6709d1284
Leonardo Mendoza <leoenmen@espol.edu.ec>
2019-12-12 04:14:28 -0500

    filtro de productos mujeres

--- angular-global-impressions/src/app/secundarias/products/products.component.ts
+++ angular-global-impressions/src/app/secundarias/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Productos } from "../../../Productos";
 import * as $ from 'jquery';
+import { TemplateDefinitionBuilder } from '@angular/compiler/src/render3/view/template';
 
 @Component({
   selector: 'app-products',
@@ -18,29 +19,43 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
     fetch('http://my-json-server.typicode.com/leomk1998/testd1/productos')
-    .then( response => response.json())
-    .then( data => {
-    let arreglo = data;
+      .then(response => response.json())
+      .then(data => {
+        let arreglo = data;
+
+        for (let objeto of arreglo) {
+          let producto: Productos = new Productos(objeto["detalle"], objeto["tipo"], objeto["precio"], objeto["image_path"])
+          document.getElementById("products").innerHTML += producto.renderizarPlantilla()
+        }
+      })
+      .catch(function (error) {
+        console.log('Hubo un problema con la petición Fetch:' + error.message);
+      });
+  }
+
+  filtrarMujer() {
+    $("#products").empty();
+    fetch('http://my-json-server.typicode.com/leomk1998/testd1/productos')
+      .then(response => response.json())
+      .then(data => {
+        let arreglo = data;
+
+        for (let objeto of arreglo) {
+          if (objeto["tipo"] == "women") {
+            let producto: Productos = new Productos(objeto["detalle"], objeto["tipo"], objeto["precio"], objeto["image_path"])
+            document.getElementById("products").innerHTML += producto.renderizarPlantilla()
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log('Hubo un problema con la petición Fetch:' + error.message);
+      });
+  }
   
-    for (let objeto of arreglo) {
-      let producto:Productos = new Productos(objeto["detalle"], objeto["tipo"], objeto["precio"],  objeto["image_path"])
-      document.getElementById("products").innerHTML += producto.renderizarPlantilla()
-  }
-})
-  .catch(function(error) {
-    console.log('Hubo un problema con la petición Fetch:' + error.message);
-  });
-  }
-
-   filtrar() {
-    this.bandera = !this.bandera;
-    this.filtro = !this.filtro;
-  }
-
 
   buscar() {
-    this.divBuscar = ! this.divBuscar;
+    this.divBuscar = !this.divBuscar;
     this.btnBuscar = !this.btnBuscar;
   }
 }
-  
+
