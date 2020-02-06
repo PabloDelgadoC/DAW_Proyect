37d3635107af6a75c5ed015f0183e39289cf5bcc
Pabelco Zambrano <pagazamb@espol.edu.ec>
2020-02-06 10:40:18 -0500

    graficos de D3, con esqueletos completos
    Falta consumir el api de historico

--- Express-API-Global-Impressions/src/api/models/historico.js
+++ Express-API-Global-Impressions/src/api/models/historico.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const HistoricoSchema = new mongoose.Schema({
   nombre: String,
-  talla: String,
   imagen: String,
   precio: Number,
   descripcion: String,
-  likes: Number
+  likes: Number,
+  autor: Number
 });
 
 module.exports = mongoose.model('historico', HistoricoSchema);
--- angular-global-impressions/dashboardadmin/src/app/lnc/line-chart/line-chart.component.ts
+++ angular-global-impressions/dashboardadmin/src/app/lnc/line-chart/line-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -9,6 +9,15 @@ import * as d3 from 'd3';
 export class LineChartComponent implements OnInit {
 
   padding = [20, 30];
+  // @Input()
+  // public title: string;
+
+  // @Input()
+  // public subtitle: string;
+
+
+  // @Input()
+  // public footerText: string;
 
   data: IData[] = [
     { date: 0, value: 0 }, 
@@ -19,14 +28,27 @@ export class LineChartComponent implements OnInit {
     { date: 50, value: 90 }, 
     { date: 80, value: 50 }
   ];
+  dataReal: IHistorico[];
 
   elementRef: ElementRef;
 
-  constructor(elementRef: ElementRef) {
-    this.elementRef = elementRef;
+  // constructor(elementRef: ElementRef) {
+  //   this.elementRef = elementRef;
+  // }
+  constructor() {
+  }
+  cosumirAPI(arr:IHistorico[]) {
+    fetch('http://localhost:3000/historico')
+      .then( response => response.json())
+      .then( data => {
+      arr = data;
+    })  
+    .catch(function(error) {
+      console.log('Hubo un problema con la petición Fetch:' + error.message);
+    });
   }
 
-  ngOnInit() {
+  makeGraph(data:IData[]){
     let svg = d3.select('#svg');
     let container = d3.select('#container');
 
@@ -58,9 +80,9 @@ export class LineChartComponent implements OnInit {
       .selectAll('text').remove();
 
     svg.append('path')
-    .datum(this.data)
+    .datum(data)
     .attr('fill', 'none')
-    .attr('stroke', 'white')
+    .attr('stroke', 'black')
     .attr('stroke-width', 1.5)
     .attr('d', d3.line<IData>()
                 .x(function(d) {return xAxis(d.date)})
@@ -69,7 +91,7 @@ export class LineChartComponent implements OnInit {
 
 
     svg.selectAll('circle-group')
-    .data(this.data)
+    .data(data)
     .enter()
     .append('circle')
       .attr('cx', d => xAxis(d.date))
@@ -78,7 +100,7 @@ export class LineChartComponent implements OnInit {
       .attr('fill', '#8FE182');
     
     svg.selectAll('vertical-lines')
-    .data(this.data)
+    .data(data)
     .enter()
     .append('rect')
       .attr('x', d => (xAxis(d.date)-45/2))
@@ -104,12 +126,150 @@ export class LineChartComponent implements OnInit {
       .on('mouseout', function() {
         container.selectAll(".tooltip").remove();
       });
-
+    
+      let svg2 = d3.select('#svg2');
+      let container2 = d3.select('#container');
+  
+  
+      svg2.append('g')
+         .attr('transform', 'translate(0,'+(height-this.padding[1])+')')
+        .call(make_x_gridlines());
+      
+      svg2.append('g')
+         .attr('transform', 'translate('+this.padding[1]+', 0)')
+         .call(d3.axisLeft(yAxis));
+  
+      svg2.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + (height - this.padding[1]) + ")")
+        .call(make_x_gridlines()
+          .tickSize(-(height - this.padding[1] - this.padding[0]))
+        ).style('color', 'grey').style('opacity', 0.2)
+        .selectAll('text').remove();
+  
+      svg2.append('path')
+      .datum(data)
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('stroke-width', 1.5)
+      .attr('d', d3.line<IData>()
+                  .x(function(d) {return xAxis(d.date)})
+                  .y(function(d) {return yAxis(d.value)})
+                  );
+  
+  
+      svg2.selectAll('circle-group')
+      .data(data)
+      .enter()
+      .append('circle')
+        .attr('cx', d => xAxis(d.date))
+        .attr('cy', d => yAxis(d.value))
+        .attr('r', 5)
+        .attr('fill', '#8FE182');
+      
+      svg2.selectAll('vertical-lines')
+      .data(data)
+      .enter()
+      .append('rect')
+        .attr('x', d => (xAxis(d.date)-45/2))
+        .attr('y', this.padding[0])
+        .attr('width', '45px')
+        .attr('height', height-this.padding[0]-this.padding[1])
+        .attr('fill', 'transparent')
+        .style('cursor', 'pointer')
+        .on('mouseover', d => {
+          container2.append('div')
+            .attr('class', 'tooltip')
+            .style('position', 'absolute')
+            .style('left', (xAxis(d.date)-50)+'px')
+            .style('top', '-20px')
+            .style('background-color', 'white')
+            .style('color', 'black')
+            .style('padding', '5px 0px')
+            .style('border-radius', '5px')
+            .style('width', '100px')
+            .style('text-align', 'center')
+            .html(`${d.value}`);
+        })
+        .on('mouseout', function() {
+          container2.selectAll(".tooltip").remove();
+        });
+        
+      let svg3 = d3.select('#svg3');
+      let container3 = d3.select('#container');
+  
+  
+      svg3.append('g')
+         .attr('transform', 'translate(0,'+(height-this.padding[1])+')')
+        .call(make_x_gridlines());
+      
+      svg3.append('g')
+         .attr('transform', 'translate('+this.padding[1]+', 0)')
+         .call(d3.axisLeft(yAxis));
+  
+      svg3.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + (height - this.padding[1]) + ")")
+        .call(make_x_gridlines()
+          .tickSize(-(height - this.padding[1] - this.padding[0]))
+        ).style('color', 'grey').style('opacity', 0.2)
+        .selectAll('text').remove();
+  
+      svg3.append('path')
+      .datum(data)
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('stroke-width', 1.5)
+      .attr('d', d3.line<IData>()
+                  .x(function(d) {return xAxis(d.date)})
+                  .y(function(d) {return yAxis(d.value)})
+                  );
+  
+  
+      svg3.selectAll('circle-group')
+      .data(data)
+      .enter()
+      .append('circle')
+        .attr('cx', d => xAxis(d.date))
+        .attr('cy', d => yAxis(d.value))
+        .attr('r', 5)
+        .attr('fill', '#8FE182');
+      
+      svg3.selectAll('vertical-lines')
+      .data(data)
+      .enter()
+      .append('rect')
+        .attr('x', d => (xAxis(d.date)-45/2))
+        .attr('y', this.padding[0])
+        .attr('width', '45px')
+        .attr('height', height-this.padding[0]-this.padding[1])
+        .attr('fill', 'transparent')
+        .style('cursor', 'pointer')
+        .on('mouseover', d => {
+          container3.append('div')
+            .attr('class', 'tooltip')
+            .style('position', 'absolute')
+            .style('left', (xAxis(d.date)-50)+'px')
+            .style('top', '-20px')
+            .style('background-color', 'white')
+            .style('color', 'black')
+            .style('padding', '5px 0px')
+            .style('border-radius', '5px')
+            .style('width', '100px')
+            .style('text-align', 'center')
+            .html(`${d.value}`);
+        })
+        .on('mouseout', function() {
+          container3.selectAll(".tooltip").remove();
+        });
 
     function make_x_gridlines() {
       return d3.axisBottom(xAxis);
     }
+  }
 
+  ngOnInit() {
+    this.makeGraph(this.data)
   }
 
 }
@@ -117,4 +277,13 @@ export class LineChartComponent implements OnInit {
 interface IData {
   date: number;
   value: number;
+}
+
+interface IHistorico{
+  nombre: String,
+  imagen: String,
+  precio: Number,
+  descripcion: String,
+  likes: Number,
+  autor: Number
 }
