6ac5caf25f647c43d9961c2476288148e9917988
Pabelco Zambrano <pagazamb@espol.edu.ec>
2020-02-06 10:42:46 -0500

    Un Cambio
    F

--- Express-API-Global-Impressions/dbMongo.js
+++ Express-API-Global-Impressions/dbMongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost:27017/globalImpeeressions';
+const URI = 'mongodb://localhost:27017/globalImpressions';
 
 mongoose.Promise = global.Promise;
 
