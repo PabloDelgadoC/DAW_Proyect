c64e18b975e370e498f0ae322afc0af6525f93dc
Leonardo Mendoza <leoenmen@espol.edu.ec>
2020-02-05 21:14:53 -0500

    login

--- angular-global-impressions/dashboardadmin/src/app/login/login.component.ts
+++ angular-global-impressions/dashboardadmin/src/app/login/login.component.ts
@@ -12,41 +12,24 @@ import { NgForm } from '@angular/forms/src/directives/ng_form';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router, private location: Location) { }
-  private user: UserInterface = {
-    user: '',
-    password: ''
-  };
-  public isError = false;
 
-  ngOnInit() { }
-
-  onLogin(form: NgForm) {
-    if (form.valid) {
-      return this.authService
-        .loginuser(this.user.user, this.user.password)
-        .subscribe(
-        data => {
-          this.authService.setUser(data.user);
-          const token = data.id;
-          this.authService.setToken(token);
-          this.router.navigate(['/user/profile']);
-          location.reload();
-          this.isError = false;
-        },
-        error => this.onIsError()
-        );
-    } else {
-      this.onIsError();
-    }
+  ngOnInit() { 
+    fetch('http://localhost:3000/usuarios')
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data);
+        let arreglo = data;
+        
+      })
+      .catch(function (error) {
+        console.log('Hubo un problema con la petición:' + error.message);
+      });
   }
 
-  onIsError(): void {
-    this.isError = true;
-    setTimeout(() => {
-      this.isError = false;
-    }, 4000);
-  }
+  onLogin() {
+    
 
+  }
 
 }
+
